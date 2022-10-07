e4bd92d8963c1205acdd027f948b64fe28cc8cee
bufferofstyx <you@example.com>
2022-10-07 05:19:26 +0100

    fixing rofi config. fixing zsh-aliases path issues. removing irrelevant sourcing for fzf from zsh-profile. Updating install script to create work and personal and repos

--- install.sh
+++ install.sh
@@ -99,5 +99,11 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/MichaelAquilina/zsh-you-should-use.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
 
+cd
+mkdir ~/repos
+mkdir ~/work
+mkdir ~/personal
+mkdir ~/work/projects
+mkdir ~/personal/projects
 
 printf "\e[1;32mYou can now reboot! Thanks you.\e[0m\n"
