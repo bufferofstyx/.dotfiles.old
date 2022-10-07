f1e001771614375e71842e47d7c3150a4fa47c79
bufferofstyx <you@example.com>
2022-10-07 04:59:49 +0100

    setting zsh as default shell

--- install.sh
+++ install.sh
@@ -93,6 +93,7 @@ sudo apt autoremove
 #Install Oh-my-zsh in ~
 cd
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc --unattended
+chsh -s $(which zsh)
 
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
