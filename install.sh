73a7bdde33e682754c3adf02b66be0e22529bcfb
bufferofstyx <you@example.com>
2022-10-07 23:51:18 +0100

    didn't fix the bug

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ sudo apt install -y exa
 sudo apt install -y meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev i3status
 sudo apt install -y dmenu rofi dunst libnotify-bin picom unzip
 sudo apt install -y vim
-sudo apt install -y nitrogen
+sudo apt install -y feh
 sudo apt install -y fonts-font-awesome fonts-powerline fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus fonts-cascadia-code
 sudo apt install -y aptitude
 
