3ad425ebf8bb011b43c3aaf60182b4c78858f2fa
bufferofstyx <51677353+bufferofstyx@users.noreply.github.com>
2022-10-23 16:41:22 +0100

    fix docker-ce

--- install.sh
+++ install.sh
@@ -100,7 +100,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt install docker-cei
+sudo apt install docker-ce
 
 #Install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/2.12.2/docker-compose-$(uname -s)-$(uname -m)"
